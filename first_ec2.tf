eb8c0d97b725fed2a4160efeb0be8f10089ee4b8
cleberzumba <81935671+cleberzumba@users.noreply.github.com>
2021-11-09 18:26:13 -0300

    Add files via upload

--- first_ec2.tf
+++ first_ec2.tf
@@ -1,8 +1,8 @@
-/*provider "aws" {
+provider "aws" {
   region = "us-west-2"
   access_key = "AKIAZ46M74IYSGWIKDT3"
   secret_key = "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"
-}*/
+}
 
 resource "aws_instance" "myec2" {
   ami = "ami-0e5b6b6a9f3db6db8"
