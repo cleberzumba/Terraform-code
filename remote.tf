19165c625e38959e7dfa5a2bc134a490d7a5a648
cleberzumba <81935671+cleberzumba@users.noreply.github.com>
2021-11-11 11:18:26 -0300

    Add files via upload

---
+++ remote.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region     = "us-west-2"
+  access_key = ""
+  secret_key = ""
+}
+
+resource "aws_instance" "myec2" {
+  ami = "ami-0e5b6b6a9f3db6db8"
+  instance_type = "t2.micro"
+  key_name = "labs-terraform"
+
+
+  provisioner "remote-exec" {
+      inline = [
+          "sudo amazon-linux-extras install -y nginx1.12",
+          "sudo systemctl start nginx"
+      ]
+
+  connection {
+      type = "ssh"
+      user = "ec2-user"
+      private_key = file("./labs-terraform.pem")
+      host = self.public_ip
+  }
+  }
+
+}
